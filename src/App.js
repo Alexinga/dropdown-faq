6d0ac1649f38fbf68fad79b5ab7e697955c7060e
Alex Inga <alexinga98@gmail.com>
2023-09-18 19:18:04 -0400

    dropdown faq

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const questions = [
+  {
+    id: 1,
+    title: "Is this a good product?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+  },
+  {
+    id: 2,
+    title: "How much does it cost?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+  },
+  {
+    id: 3,
+    title: "When can I get it?",
+    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="faq-container">
+      <h1>Frequently Asked Questions</h1>
+      <Accordion></Accordion>
     </div>
   );
 }
 
+function Accordion() {
+  return (
+    <ul className="acc-ul">
+      {questions.map((question) => (
+        <AccordionItem key={question.id} currQues={question}></AccordionItem>
+      ))}
+    </ul>
+  );
+}
+
+function AccordionItem({ currQues }) {
+  const [isOpen, setIsOpen] = useState(true);
+  function handleOpen() {
+    setIsOpen(!isOpen);
+  }
+  return (
+    <li>
+      <div className="acc-item">
+        <h2>{currQues.title}</h2>
+        <p onClick={handleOpen}>{isOpen ? "+" : "-"}</p>
+      </div>
+      {!isOpen && <p className="desc">{currQues.info}</p>}
+    </li>
+  );
+}
 export default App;
